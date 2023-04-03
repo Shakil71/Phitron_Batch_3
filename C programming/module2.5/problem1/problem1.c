fefa571cf5f933229f124708ddaea9c89d904fdf
Shakil71 <mdalaminshakil31@gmail.com>
2023-04-04 00:08:12 +0600

    problem1

---
+++ C programming/module2.5/problem1/problem1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+   printf("Recently I heard that you’ve achieved \n 95%% marks in your exam.\n");
+   printf("This is brilliant! ");
+   printf("I wish you’ll shine in your life!\t\t");
+   printf("Good\n luck with all the barriers(/\\) in your life.");
+   return 0;
+}
