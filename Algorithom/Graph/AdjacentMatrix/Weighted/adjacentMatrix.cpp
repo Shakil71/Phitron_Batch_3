ace5b545502e22eef3b16c989c4cf84da6aef81f
Shakil71 <mdalaminshakil31@gmail.com>
2023-09-07 13:08:12 +0600

    Algorithom

---
+++ Algorithom/Graph/AdjacentMatrix/Weighted/adjacentMatrix.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e3+5;
+int adjMat[N][N];
+int main()
+{
+   int n,m;
+   cin >> n >> m;
+   for (int i = 0; i < m; i++)
+   {
+    int u,v,w;
+    cin >> u >> v >> w;
+    adjMat[u][v] = w;
+    adjMat[v][u] = w;
+   }
+   for (int i = 1; i <= n; i++)
+   {
+    /* code */
+    for (int j = 1; j <= n; j++)
+    {
+        /* code */
+        cout << adjMat[i][j] <<  " ";
+    }
+    cout << endl;
+    
+   }
+   
+    
+   return 0;
+}
